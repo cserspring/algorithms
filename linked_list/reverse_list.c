1dbba408632ba45569532fd5b4431b85b8ccb64a
Shuai Wang <shuaiwhu@gmail.com>
2013-10-03 01:08:32 -0400

    reverse list pointer's pointer

---
+++ linked_list/reverse_list.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+typedef struct node {
+    int val;
+    struct node *next;
+} node_t;
+
+/* Pass pointer's pointer */
+void reverse_list(node_t **root)
+{
+    if (root) {
+        node_t *prev = *root;
+        node_t *current = (*root)->next;
+        prev->next = NULL;
+        node_t *tmp;
+        while (current) {
+            tmp = prev;
+            prev = current;
+            current = current->next;
+            prev->next = tmp;
+        }
+        *root = prev;
+    }
+}
+
+int main()
+{
+    node_t node1;
+    node_t node2;
+    node_t root;
+    node2.val = 6;
+    node2.next = NULL;
+    node1.val = 5;
+    node1.next = &node2;
+    root.val = 4;
+    root.next = &node1;
+    
+    node_t *head = &root;
+    reverse_list(&head);
+    while (head) {
+        printf("%d\n", head->val);
+        head = head->next;
+    }
+}
---
+++ symbol_and.c
@@ -0,0 +1,17 @@
+﻿/* Actually, this program calculates the number of odd bits 
+ * in a binary number 
+ */
+#include <stdio.h>
+
+int main()
+{
+    int count = 0;
+    int countx = 9999;
+
+    while(countx)
+    {
+        count++;
+        countx = countx&(countx - 1);
+    }
+    printf("%d\n", count);
+}
