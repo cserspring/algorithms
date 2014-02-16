4142bf7e6b428aace02eb78a0edd136d3e8066ff
Shuai Wang <shuaiwhu@gmail.com>
2014-02-16 18:38:53 -0500

    find the snake in the matrix

---
+++ dp/matrix_snake.c
@@ -0,0 +1,63 @@
+/*
+ You are given a grid of numbers. A snake sequence is made up of adjacent 
+ numbers such that for each number, the number on the right or the number 
+ below it is +1 or -1 its value. For example, 
+
+ 1  3 2  6 8 
+ -9 7 1 -1 2 
+ 1  5 0  1 9 
+
+ In this grid, (3, 2, 1, 0, 1) is a snake sequence. 
+
+ Given a grid, find the longest snake sequences and their length
+ */
+#include <stdio.h>
+#include <math.h>
+
+#define M 3
+#define N 5
+
+int matrix[M][N] = 
+{
+1,3,2,6,8,
+-9,7,1,-1,2,
+1,5,0,1,2
+};
+
+int dp[M][N];
+int get_max()
+{
+    int i, j;
+    for (i = 0; i < M; ++i) {
+        for (j = 0; j < N; ++j) {
+            dp[i][j] = 1;
+        }
+    }
+
+    for (i = 0; i < M; ++i) {
+        for (j = 0; j < N; ++j) {
+            if ((j+1 < N) && abs(matrix[i][j+1]-matrix[i][j]) == 1){
+                dp[i][j+1] = (dp[i][j+1] > (dp[i][j]+1)) ? dp[i][j+1] : (dp[i][j]+1);
+            }
+            if ((i+1 < M) && abs(matrix[i+1][j]-matrix[i][j]) == 1) {
+                dp[i+1][j] = (dp[i+1][j] > (dp[i][j]+1)) ? dp[i+1][j] : (dp[i][j]+1);
+            }
+        }
+    }
+
+    int max = 0;
+    for (i = 0; i < M; ++i) {
+        for (j = 0; j < N; ++j) {
+            printf("%d\t", dp[i][j]);
+            if (dp[i][j] > max)
+                max = dp[i][j];
+        }
+        printf("\n");
+    }
+    return max;
+}
+
+int main()
+{
+    printf("%d\n", get_max());
+}
